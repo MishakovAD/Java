b4f388aadd6a45a021117e6040248ec163fbecb0
MishakovAD <36512496+MishakovAD@users.noreply.github.com>
2018-08-06 16:07:40 +0300

    a start to work with excel
    Начало положено работы с экселем. Сервлеты, отвечающие за "первые шаги" готовы, Работа с БД начата. Осталась настройка красоты и куча разных мелочей, чтобы все работало как надо.

--- niikp/src/main/java/ExcelApachePOI/IncomingMailExcel.java
+++ niikp/src/main/java/ExcelApachePOI/IncomingMailExcel.java
@@ -1,5 +1,6 @@
 package ExcelApachePOI;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -16,53 +17,77 @@ import org.apache.poi.ss.usermodel.DataFormat;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class IncomingMailExcel {
 	
 	@SuppressWarnings("deprecation")
     public static void writeIntoExcel(String file) throws FileNotFoundException, IOException{
-        Workbook book = new HSSFWorkbook();
-        Sheet sheet = book.createSheet("Birthdays");
-
-        // Нумерация начинается с нуля
-        Row row = sheet.createRow(0); 
+		XSSFWorkbook book;
+		File f = new File(file);
+		if(f.exists() && !f.isDirectory()) { 
+			book = new XSSFWorkbook(new FileInputStream(file)); //дозапись в уже существующий
+		} else {
+			book = new XSSFWorkbook(); //создание нового файла
+		}
         
-        // Мы запишем имя и дату в два столбца
-        // имя будет String, а дата рождения --- Date,
-        // формата dd.mm.yyyy
-        Cell name = row.createCell(0);
-        name.setCellValue("John");
+        Sheet sheet;
+        if (book.getSheet("incomingMail") != null) {
+        	sheet = book.getSheet("incomingMail");
+        	System.out.println("get");
+        } else {
+        	sheet = book.createSheet("incomingMail");
+        	System.out.println("create");
+        }
         
-        Cell birthdate = row.createCell(1);
         
-        DataFormat format = book.createDataFormat();
-        CellStyle dateStyle = book.createCellStyle();
-        dateStyle.setDataFormat(format.getFormat("dd.mm.yyyy"));
-        birthdate.setCellStyle(dateStyle);
         
- 
-        // Нумерация лет начинается с 1900-го
-        birthdate.setCellValue(new Date(110, 10, 10));
+        start: 
+        	for (int i = 0; ; i++) {
+        	// Нумерация начинается с нуля             
+            int lastRowNum = sheet.getLastRowNum();
+            Row row = sheet.createRow(lastRowNum+1);
+            
+            Cell name = row.createCell(0);
+            name.setCellValue("John");
+            
+            Cell birthdate = row.createCell(1);
+            
+            DataFormat format = book.createDataFormat();
+            CellStyle dateStyle = book.createCellStyle();
+            dateStyle.setDataFormat(format.getFormat("dd.mm.yyyy"));
+            birthdate.setCellStyle(dateStyle);
+            
+     
+            // Нумерация лет начинается с 1900-го
+            birthdate.setCellValue(new Date(110, 10, 10));
+            
+            // Меняем размер столбца
+            sheet.autoSizeColumn(1);
+            book.write(new FileOutputStream(file));
+            break start;
+        }
         
-        // Меняем размер столбца
-        sheet.autoSizeColumn(1);
         
         // Записываем всё в файл
-        book.write(new FileOutputStream(file));
+        
         book.close();
     }
 	
 	public static void readFromExcel(String file) throws IOException{
-        HSSFWorkbook myExcelBook = new HSSFWorkbook(new FileInputStream(file));
-        HSSFSheet myExcelSheet = myExcelBook.getSheet("Birthdays");
-        HSSFRow row = myExcelSheet.getRow(0);
+        XSSFWorkbook myExcelBook = new XSSFWorkbook(new FileInputStream(file));
+        XSSFSheet myExcelSheet = myExcelBook.getSheet("Birthdays");
+        XSSFRow row = myExcelSheet.getRow(0);
         
-        if(row.getCell(0).getCellType() == HSSFCell.CELL_TYPE_STRING){
+        if(row.getCell(0).getCellType() == XSSFCell.CELL_TYPE_STRING){
             String name = row.getCell(0).getStringCellValue();
             System.out.println("name : " + name);
         }
         
-        if(row.getCell(1).getCellType() == HSSFCell.CELL_TYPE_NUMERIC){
+        if(row.getCell(1).getCellType() == XSSFCell.CELL_TYPE_NUMERIC){
             Date birthdate = row.getCell(1).getDateCellValue();
             System.out.println("birthdate :" + birthdate);
         }
@@ -70,5 +95,12 @@ public class IncomingMailExcel {
         myExcelBook.close();
         
     }
-	
+	public static void main(String[] args) {
+		String filename = "E:/JavaProjectDocs/excel/incomingMail.xlsx";
+		try {
+			writeIntoExcel(filename);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
