4441f5ecded249baa2acfd1ff53ba8220515f386
MishakovAD <36512496+MishakovAD@users.noreply.github.com>
2018-09-20 16:23:07 +0300

    v3.2.1
    Удалена ненужна джаваскрипт библиотека

--- niikp/src/main/webapp/resources/js/jsFile-master/src/libs/polyfills/string.js
+++
@@ -1,5 +0,0 @@
-export default () => {
-    String.prototype.includes = String.prototype.includes || function () {
-        return this.indexOf.apply(this, arguments) !== -1;
-    };
-};
--- niikp/src/main/webapp/resources/js/jsFile-master/src/utils/clone.js
+++
@@ -1,5 +0,0 @@
-import merge from './merge';
-
-export default function (obj) {
-    return merge({}, obj);
-}
